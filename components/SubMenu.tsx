125a651ba44cf6c9c19716543e17bdbec26dae9f
poppychan21 <knguyen19509@gmail.com>
2024-08-08 23:10:41 -0700

    Updated files (login, signup and submenu)
    added:
    - modal for login and signup
    - "already have an account" feature

--- components/SubMenu.tsx
+++ components/SubMenu.tsx
@@ -1,7 +1,10 @@
-import { Modal, View, StyleSheet, Pressable} from "react-native";
+import { Modal, View, StyleSheet, Pressable } from "react-native";
 import { Drawer } from 'react-native-paper';
 import { SubMenuNavigationProp } from "@/constants/types"; // this is the identity
 import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import LogInScreen from "@/app/(Auth)/LogIn";
+import SignUpScreen from "@/app/(Auth)/SignUp";
 
 type SubMenuProps = {
     isVisible: boolean,
@@ -9,58 +12,72 @@ type SubMenuProps = {
 }
 
 // make this sub menu be set to the left side of the screen 
-const SubMenu= (props : SubMenuProps) => {
+const SubMenu = (props: SubMenuProps) => {
     const navigation = useNavigation<SubMenuNavigationProp>(); // this gives you access to the hook
+    const [isLogInVisible, setLogInVisible] = useState(false);
+    const [isSignUpVisible, setSignUpVisible] = useState(false);
 
     return (
         <Modal
-          transparent={true}
-          animationType="none"
-          visible={props.isVisible}
-          onRequestClose={props.onClose}
+            transparent={true}
+            animationType="none"
+            visible={props.isVisible}
+            onRequestClose={props.onClose}
         >
             <Pressable style={styles.container} onPress={props.onClose}>
-                    <Pressable style={styles.left_side} onPress={(e) => e.stopPropagation()}>
-                            <Drawer.Section title="MENU">
-                                <Drawer.Item
-                                    style={[styles.box, styles.TextSpace]}
-                                    label="Log in"
-                                    onPress={() => {
-                                        navigation.navigate('LogInScreen')
-                                        console.log("Pressed")
-                                    }}
-                                />
-                                <Drawer.Item
-                                    style={[styles.box, styles.BigSpace]}
-                                    label="Sign up"
-                                    onPress={() => {
-                                        navigation.navigate('SignUpScreen')
-                                        console.log("Pressed")
-                                    }}
-                                />
-                                <Drawer.Item
-                                    style={[styles.box, styles.TextSpace]}
-                                    label="New"
-                                    onPress={() => { }}
-                                />
-                                <Drawer.Item
-                                    style={[styles.box, styles.TextSpace]}
-                                    label="Save"
-                                    onPress={() => { }}
-                                />
-                                <Drawer.Item
-                                    style={[styles.box, styles.TextSpace]}
-                                    label="Save & Exit"
-                                    onPress={() => { }}
-                                />
-                                <Drawer.Item
-                                    style={[styles.box, styles.TextSpace]}
-                                    label="Exit"
-                                    onPress={() => { }}
-                                />
-                            </Drawer.Section>
-                    </Pressable>
+                <Pressable style={styles.left_side} onPress={(e) => e.stopPropagation()}>
+                    <Drawer.Section title="MENU">
+                        <Drawer.Item
+                            style={[styles.box, styles.TextSpace]}
+                            label="Log in"
+                            onPress={() => {
+                                setLogInVisible(!isLogInVisible); // this shows LogInScreen modal
+                                console.log("Login Pressed")
+                            }}
+                        />
+                        <Drawer.Item
+                            style={[styles.box, styles.BigSpace]}
+                            label="Sign up"
+                            onPress={() => {
+                                setSignUpVisible(!isSignUpVisible); // this shows SignUpScreen modal
+                                console.log("Signup Pressed")
+                            }}
+                        />
+                        <Drawer.Item
+                            style={[styles.box, styles.TextSpace]}
+                            label="New"
+                            onPress={() => { }}
+                        />
+                        <Drawer.Item
+                            style={[styles.box, styles.TextSpace]}
+                            label="Save"
+                            onPress={() => { }}
+                        />
+                        <Drawer.Item
+                            style={[styles.box, styles.TextSpace]}
+                            label="Save & Exit"
+                            onPress={() => { }}
+                        />
+                        <Drawer.Item
+                            style={[styles.box, styles.TextSpace]}
+                            label="Exit"
+                            onPress={() => { }}
+                        />
+                    </Drawer.Section>
+                </Pressable>
             </Pressable>
+
+            {/* LogInScreen Modal */}
+            <LogInScreen
+                isVisible={isLogInVisible}
+                onClose={() => setLogInVisible(false)}
+            />
+
+            {/* SignUpScreen Modal */}
+            <SignUpScreen
+                isVisible={isSignUpVisible}
+                onClose={() => setSignUpVisible(false)}
+            />
         </Modal>
     );
 };
@@ -97,7 +114,3 @@ const styles = StyleSheet.create({
 });
 
 export default SubMenu;
-/*
-Self-Note:
-change the font size smaller(?)
-*/
