e7ad4af4a421dd4e0a0c17cf5cea91f954ba11d1
Honghui-Li-8 <honghuili8@gmail.com>
2024-09-03 17:10:52 -0700

    stage, unfixed

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-typescript"
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       'module:metro-react-native-babel-preset',
+//       "@babel/preset-env",
+//       "@babel/preset-react",
+//       "@babel/preset-typescript"
+//     ],
+//     plugins: [
+//       ["@babel/plugin-transform-class-properties", { "loose": true }],
+//       ["@babel/plugin-transform-private-methods", { "loose": true }],
+//       ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
+//     ]
+//   };
+// };
